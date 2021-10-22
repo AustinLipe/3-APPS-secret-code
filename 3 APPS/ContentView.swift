695045ce2a6586fcddbe5b0f14e861e496d7e9b2
AustinLipe <90396750+AustinLipe@users.noreply.github.com>
2021-10-22 08:22:58 -0500

    Initial Commit

---
+++ 3 APPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  3 APPS
+//
+//  Created by AMStudent on 10/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 3 APPS/__APPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  __APPSApp.swift
+//  3 APPS
+//
+//  Created by AMStudent on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct __APPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
